ff0420445c636bfeda7b5b13909b2c33de2ac86c
VovaSewer <shutko7@gmail.com>
2022-02-03 20:49:24 +0200

    resubmit

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,30 +1,44 @@
 import throttle from 'lodash.throttle'
 
 const feedBackFormRef = document.querySelector('.feedback-form');
+const STORAGE_KEY = 'feedback-form-state';
 
-feedBackFormRef.addEventListener('input', throttle(onFormInput, 500));
 feedBackFormRef.addEventListener('submit', onFormSubmit);
+feedBackFormRef.addEventListener('input', throttle(onFormInput, 500));
 
 saveForm();
 
 function onFormInput (event) {
     if (event.currentTarget) {
-        localStorage.setItem("feedback-form-state", JSON.stringify({email: event.currentTarget.email.value, message: event.currentTarget.message.value}));
+
+        localStorage.setItem(STORAGE_KEY, JSON.stringify({email: event.currentTarget.email.value, message: event.currentTarget.message.value}));
     };
 };
 
 function onFormSubmit(event) {
     event.preventDefault();
-    console.log(JSON.parse(localStorage.getItem("feedback-form-state")));
-    localStorage.removeItem("feedback-form-state");
+
+    console.log(JSON.parse(localStorage.getItem(STORAGE_KEY)));
+    localStorage.removeItem(STORAGE_KEY);
+
+    const formElements = event.currentTarget.elements;
+    console.log(formElements);
+
+    const email = formElements.email.value;
+    const message = formElements.message.value;
+
     event.currentTarget.reset();
+
+    if (email === '' || message === '') {
+        alert ('Заполни все поля перед отправкой')
+    }
+
 };    
 
 function saveForm() {
-    const savedFormData = JSON.parse(localStorage.getItem("feedback-form-state"));
+    const savedFormData = JSON.parse(localStorage.getItem(STORAGE_KEY));
 
     if(savedFormData) {
-        
         feedBackFormRef.elements.email.value = savedFormData.email;
         feedBackFormRef.elements.message.value = savedFormData.message;
     }
